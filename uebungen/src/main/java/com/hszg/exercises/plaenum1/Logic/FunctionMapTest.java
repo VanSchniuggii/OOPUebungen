911fca2d946b6331040d79acca25b4146b172858
VanSchniuggii <postfach-alex@kurort-oybin.com>
2026-03-26 12:49:59 +0100

    Added Another Function + Tests

--- uebungen/src/main/java/com/hszg/exercises/plaenum1/Logic/FunctionMapTest.java
+++ uebungen/src/main/java/com/hszg/exercises/plaenum1/Logic/FunctionMapTest.java
@@ -4,11 +4,14 @@ public class FunctionMapTest {
 
     public static void main(String[] args) {
         Function func = new FunctionPlusFive();
+        Function func2 = new FunctionModulo2();
         FunctionMap fTest = new FunctionMap();
         int[] input = {1, 2, 3, 4, 5};
         int[] expectedOutput = {6, 7, 8, 9, 10};
         int[] output = fTest.map(input, func);
         boolean success = true;
+
+        System.out.println("Testing FunctionPlusFive...");
         for (int i = 0; i < input.length; i++) {
             if (output[i] != expectedOutput[i]) {
                 success = false;
@@ -21,6 +24,24 @@ public class FunctionMapTest {
         } else {
             System.out.println("Test failed!");
         }
+
+        System.out.println("Testing FunctionModulo2...");
+        int[] expectedOutput2 = {1, 0, 1, 0, 1};
+        int[] output2 = fTest.map(input, func2);
+        success = true;
+        for (int i = 0; i < input.length; i++) {
+            if (output2[i] != expectedOutput2[i]) {
+                success = false;
+                System.out.println("Test failed at index " + i + ": expected " + (expectedOutput2[i]) + " but got " + output2[i]);
+                break;
+            }
+        }
+        if (success) {
+            System.out.println("Test passed! Output: " + java.util.Arrays.toString(output2));
+        } else {
+            System.out.println("Test failed!");
+        }
+
     }
 
 }
---
+++ uebungen/src/main/java/com/hszg/exercises/plaenum1/Logic/FunctionModulo2.java
@@ -0,0 +1,10 @@
+package uebungen.src.main.java.com.hszg.exercises.plaenum1.Logic;
+
+public class FunctionModulo2 implements Function {
+
+    @Override
+    public int execute(int x) {
+        return x % 2;
+    }
+
+}
