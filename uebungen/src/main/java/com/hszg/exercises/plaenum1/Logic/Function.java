b4430d82e3272b195b9a58c6f7fb6406d5bdaca7
VanSchniuggii <postfach-alex@kurort-oybin.com>
2026-03-26 12:51:48 +0100

    Added Doc to Function Interface

--- uebungen/src/main/java/com/hszg/exercises/plaenum1/Logic/Function.java
+++ uebungen/src/main/java/com/hszg/exercises/plaenum1/Logic/Function.java
@@ -1,7 +1,16 @@
 package uebungen.src.main.java.com.hszg.exercises.plaenum1.Logic;
 
+/**
+ * Represents a function that maps an integer input to an integer output.
+ */
 public interface Function {
 
+    /**
+     * Executes the function for the given input value.
+     *
+     * @param x the input value
+     * @return the calculated result
+     */
     int execute(int x);
 
 }
